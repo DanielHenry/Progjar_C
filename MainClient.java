499992162041996ef74ca0a3b832b9a53cb2f2d3
Ardhinata Juari <ardhinata.juari@gmail.com>
2014-11-27 21:03:23 +0700

    Make Digest

--- MainClient.java
+++ MainClient.java
@@ -1,4 +1,20 @@
 import java.io.*;
 import java.util.*;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class MainClient{
+    
+    public static void main(String[] args) {
+        try {
+            /* Digest Initialization */
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            /* TODO: Add initialization here */
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(MainClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
