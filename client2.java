d13029de1d0dc318678dbcb23557acb031096a34
Muhammad Wildianurahman <mr_driver_wildi@yahoo.co.id>
2014-11-27 23:17:58 +0700

    Update client2.java

--- client2.java
+++ client2.java
@@ -1,11 +1,31 @@
+package clienttorrent;
+
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 import java.net.Socket;
 
-class utama
-{
-  public static void main(String[] argv)
-  {
+/* @author Wildi*/
+public class ClientTorrent {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String m;
+        Socket client = new Socket(); /*didalam kurung ada ip, port*/
+        InputStream r = client.getInputStream();
+        OutputStream w = client.getOutputStream();
+        for(;true;){
+            m = scan.nextLine();
+            w.write(m.getBytes()); /*isi didalam kurung adalah byte kalimat yang mau diisi*/
+            for(;true;){
+                int temp = r.read();
+                if(temp == -1)
+                    break;
+                else{
+                    char a = (char)temp;
+                    System.out.print(a);
+                }
+            }   
+        }            
+    }
     
-  }
 }
