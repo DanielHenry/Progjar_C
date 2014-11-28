17be16a956f7cd49380c54ee80b84864d583cb7f
Ardhinata Juari <ardhinata.juari@gmail.com>
2014-11-28 22:36:14 +0700

    Organize all files into netbeans project
    Semua file masuk ke folder project ini. Biar rapi.

---
+++ FileShare/src/FShareServer/ServerMain.java
@@ -0,0 +1,29 @@
+package FShareServer;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.*;
+
+public class ServerMain
+{
+	public static void main(String[] argv)
+	{
+		try
+		{
+			while (true)
+			{
+				ServerSocket serversocket = new ServerSocket(1234);
+				Socket clientsocket = serversocket.accept();
+				ThreadServer thread = new ThreadServer(clientsocket);
+				thread.run();
+			}
+		}
+		catch(IOException e)
+		{
+			System.out.println("Ada yang salah dengan semua ini dari server utama!!!");
+		}
+	}
+}
